558918756e2dca60d9671a6373a4104dfff7be04
nusususuzu <nusususuzu@users.noreply.github.com>
2016-01-29 14:43:49 +0200

    Update cl-app.js

--- cl-app.js
+++ cl-app.js
@@ -1,17 +1,16 @@
 #! /usr/bin/env node
 var tureng = require('./tureng.js'),
-	Table  = require('cli-table');
+    Table  = require('cli-table');
 tureng(process.argv[2], function(error, data) {
 	var table = new Table({
-		head:['Category','English','Turkish'],
-		colWidths: [20,20,20]
+		head:['Category','English','Turkish']
 	}) ;
 	if(error) {
 		table.push([error.message, '']);
 	} else {
 		data.categories.forEach(function (category) {
 			category.results.forEach(function (result) {
-				table.push([category.name ,result.type +' '+ result.english, result.turkish]);
+				table.push([category.name, result.type +' '+ result.english, result.turkish]);
 			});
 		});
 	}
